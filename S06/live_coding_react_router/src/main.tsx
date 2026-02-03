97726a97a0f93cca0492e849ac458c9dce511401
YoussefSlim <slimyoussef9511@gmail.com>
2026-02-03 13:54:34 +0100

    live coding react router v6.4

--- S06/live_coding_react_router/src/main.tsx
+++ S06/live_coding_react_router/src/main.tsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorBoundary from "./components/error/ErrorBoundary.tsx";
+import Home from "./components/home/Home.tsx";
+import Layout from "./components/layout/Layout.tsx";
+import Login from "./components/login/Login.tsx";
+import Form from "./components/form/Form.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorBoundary />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "signup",
+        element: <Form />,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
